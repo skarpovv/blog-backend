170021759fdb4ec03ee66f2ca9d0c7e616c4efbb
Serghei Carpov <c.serghei@bend.md>
2023-02-14 15:46:39 +0200

    test login

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { authController } from '../controllers/auth.controller';
 import { check } from 'express-validator';
 import { registerValidators } from './validation';
+import { loginValidators } from './validation/auth/login.validators';
 
 const router = Router();
 
@@ -10,7 +11,11 @@ router.post(
   registerValidators,
   authController.register.bind(authController)
 );
-router.post('/login', authController.login.bind(authController));
-router.get('/users', authController.getUsers.bind(authController));
+router.post(
+  '/login',
+  loginValidators,
+  authController.login.bind(authController)
+);
+// router.get('/users', authController.getUsers.bind(authController));
 
 export default router;
